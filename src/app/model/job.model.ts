02b243d0e79e786d075eddd607dda181f9b5bd4c
Nguyễn Hữu Phúc <phucnguyen19011997@gmail.com>
2019-04-06 16:41:22 +0700

    phucnh addJob

---
+++ src/app/model/job.model.ts
@@ -0,0 +1,13 @@
+export class Job {
+  id: number;
+  email: string;
+  titleJob: string;
+  location: string;
+  major: string; //chuyen ngành
+  requireCadiate: string; // yêu cầu kĩ năng ứng viên
+  requireYear: number; // yêu cầu số năm kinh nghiệm
+  salary: number;
+  description: string; // mô tả công việc
+  duedate: string; // hạn apply
+
+}
