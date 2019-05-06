7e2f90755a2d0a9dc2ea49bc78d4082da0b4456e
dhieu.pham@gmail.com <dhieu.pham@gmail.com>
2019-05-06 08:31:42 +0700

    commit

--- src/app/manage-recruitment/manage-recruitment.component.ts
+++ src/app/manage-recruitment/manage-recruitment.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {JobService} from "../service/job.service";
+import {Job} from "../model/job.model";
+import {Account} from "../model/account.model";
+import {first} from "rxjs/operators";
 
 @Component({
   selector: 'app-manage-recruitment',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageRecruitmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private jobService: JobService,
+  ) { }
 
-  ngOnInit() {
-  }
+    page: number;
+    pageSize: number;
+    listJob: Job[];
+    account = new Account();
+    job = new Job();
+    ngOnInit() {
+        this.account = JSON.parse(localStorage.getItem('currentUser'));
+        this.page = 1;
+        this.pageSize = 5;
+        this.job.trangThai = "0";
+        this.jobService.findByTrangThai(JSON.stringify(this.job))
+            .pipe(first())
+            .subscribe(
+                (data: Job[]) => {
+                    this.listJob = data;
+                    console.log(this.listJob);
+                },
+                error => {
+                    console.log('Failed');
+                }
+            );
+    }
 
+    onConfirmed(){
+
+    }
+
+    onRejected(){
+
+    }
 }
