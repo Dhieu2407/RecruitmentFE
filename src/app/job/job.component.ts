0aacdf201d3dbb9de9aa86d640bed83eb858e572
magnet <tgn296@gmail.com>
2019-05-25 15:14:58 +0700

    different between unsave and save, apply vs unapply

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,121 +1,151 @@
-import { Component, OnInit } from '@angular/core';
-import { Job } from '../model/job.model';
-import { JobService } from '../service/job.service';
-import { first } from 'rxjs/operators';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { SearchJob } from '../model/searchJob.model';
-import {Router} from '@angular/router';
-import {Major} from "../model/major.model";
-import { CandidateService } from '../service/candidate.service';
-import { CandidateSaveJobsDTO } from '../model/candidateSaveJobsDTO.model';
-import { MajorService } from '../service/major.service';
+import { Component, OnInit } from "@angular/core";
+import { Job } from "../model/job.model";
+import { JobService } from "../service/job.service";
+import { first } from "rxjs/operators";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { SearchJob } from "../model/searchJob.model";
+import { Router } from "@angular/router";
+import { Major } from "../model/major.model";
+import { CandidateService } from "../service/candidate.service";
+import { CandidateSaveJobsDTO } from "../model/candidateSaveJobsDTO.model";
+import { MajorService } from "../service/major.service";
 
 @Component({
-  selector: 'app-job',
-  templateUrl: './job.component.html',
-  styleUrls: ['./job.component.css']
+    selector: "app-job",
+    templateUrl: "./job.component.html",
+    styleUrls: ["./job.component.css"]
 })
 export class JobComponent implements OnInit {
+    constructor(private formBuilder: FormBuilder, private router: Router, private jobService: JobService, private candidateService: CandidateService, private majorService: MajorService) {}
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private router: Router,
-    private jobService: JobService,
-    private candidateService: CandidateService,
-    private majorService: MajorService
-  ) { }
+    searchJobForm: FormGroup;
+    listJobs: Job[];
+    job = new Job();
+    listMajor: Major[];
+    page: number;
+    pageSize: number;
+    searchJob = new SearchJob();
+    id: number;
+    savedJobs : Job[];
+    appliedJobs : Job[];
+    savedJobsId = [];
+    appliedJobsId = [];
 
-  searchJobForm: FormGroup;
-  listJobs: Job[];
-  job = new Job();
-  listMajor: Major[];
-  page: number;
-  pageSize: number;
-  searchJob = new SearchJob();
+    candidateSaveJobDTO: CandidateSaveJobsDTO;
 
-  candidateSaveJobDTO : CandidateSaveJobsDTO;
-
-  onSubmit(buttonType): void {
-    if (buttonType === 'Search') {
-      if (this.searchJobForm.get('keyword').value === '' &&
-        this.searchJobForm.get('location').value === '' &&
-        this.searchJobForm.get('career').value === '') {
-        this.jobService.getAllJobs()
-          .pipe(first())
-          .subscribe(
-            (data: Job[]) => {
-              this.listJobs = data;
-              console.log(this.listJobs);
-            },
-            error => {
-              console.log('Fail');
+    onSubmit(buttonType): void {
+        if (buttonType === "Search") {
+            if (this.searchJobForm.get("keyword").value === "" && this.searchJobForm.get("location").value === "" && this.searchJobForm.get("career").value === "") {
+                this.jobService
+                    .getAllJobs()
+                    .pipe(first())
+                    .subscribe(
+                        (data: Job[]) => {
+                            this.listJobs = data;
+                            console.log(this.listJobs);
+                        },
+                        error => {
+                            console.log("Fail");
+                        }
+                    );
+            } else {
+                this.searchJob.tenJob = this.searchJobForm.get("keyword").value;
+                this.searchJob.diaChi = this.searchJobForm.get("location").value;
+                this.searchJob.tenNganh = this.searchJobForm.get("career").value;
+                this.searchJob.tenCongTy = "";
+                console.log(this.searchJob);
+                this.jobService
+                    .searchJobs(JSON.stringify(this.searchJob))
+                    .pipe(first())
+                    .subscribe(
+                        (data: Job[]) => {
+                            this.listJobs = [];
+                            this.listJobs = data;
+                            console.log(this.listJobs);
+                        },
+                        error => {
+                            console.log("Fail");
+                        }
+                    );
             }
-          );
-      } else {
-        this.searchJob.tenJob = this.searchJobForm.get('keyword').value;
-        this.searchJob.diaChi = this.searchJobForm.get('location').value;
-        this.searchJob.tenNganh = this.searchJobForm.get('career').value;
-        this.searchJob.tenCongTy = '';
-        console.log(this.searchJob);
-        this.jobService.searchJobs(JSON.stringify(this.searchJob))
-          .pipe(first())
-          .subscribe(
-            (data: Job[]) => {
-              this.listJobs = [];
-              this.listJobs = data;
-              console.log(this.listJobs);
-            },
-            error => {
-              console.log('Fail');
-            }
-          );
-      }
+        }
     }
-  }
 
-  goToProductDetails(id) {
-    this.router.navigate(['/jobdetail/:id', id]);
-  }
+    goToProductDetails(id) {
+        this.router.navigate(["/jobdetail/:id", id]);
+    }
 
-  ngOnInit() {
-      this.page = 1;
-      this.pageSize = 10;
-      this.searchJobForm = this.formBuilder.group({
-      keyword: ['', Validators.required],
-      location: ['', Validators.required],
-      career: ['', Validators.required],
-    });
-      this.majorService.getAllMajors()
-          .pipe(first())
-          .subscribe(
-              (data: Major[]) => {
-                  this.listMajor = data;
-              },
-              error => {
-                  console.log('Fail');
-              }
-          );
+    ngOnInit() {
+        this.page = 1;
+        this.pageSize = 10;
+        this.id = JSON.parse(localStorage.getItem("currentUser")).id;
+        this.searchJobForm = this.formBuilder.group({
+            keyword: ["", Validators.required],
+            location: ["", Validators.required],
+            career: ["", Validators.required]
+        });
+        this.majorService
+            .getAllMajors()
+            .pipe(first())
+            .subscribe(
+                (data: Major[]) => {
+                    this.listMajor = data;
+                },
+                error => {
+                    console.log("Fail");
+                }
+            );
 
-      this.job.trangThai = "1";
-      this.jobService.findByTrangThai(JSON.stringify(this.job))
-          .pipe(first())
-          .subscribe(
-              (data: Job[]) => {
-                  this.listJobs = data;
-                  console.log(this.listJobs);
-              },
-              error => {
-                  console.log('Failed');
-              }
-          );
-  }
-  onBookmark(jobs: Job) {
-    this.candidateSaveJobDTO = new CandidateSaveJobsDTO();
-    this.candidateSaveJobDTO.candidateId =  JSON.parse(localStorage.getItem("currentUser")).id;
-    console.log(jobs);
-    this.candidateSaveJobDTO.jobId =  jobs.jobId;
-    console.log(this.candidateSaveJobDTO);
-    this.candidateService.candidateSaveJob(this.candidateSaveJobDTO)
+        this.job.trangThai = "1";
+        this.jobService
+            .findByTrangThai(JSON.stringify(this.job))
+            .pipe(first())
+            .subscribe(
+                (data: Job[]) => {
+                    this.listJobs = data;
+                    console.log(this.listJobs);
+                },
+                error => {
+                    console.log("Failed");
+                }
+            );
+        this.candidateService.getBookmarkedJob(this.id)
+            .pipe(first())
+            .subscribe(
+                (data: Job[]) => {
+                    this.savedJobs = data;
+                    for(var i = 0 ; i < this.savedJobs.length ; ++i) {
+                        this.savedJobsId.push(this.savedJobs[i].jobId);
+                    }
+                    console.log(this.savedJobsId);
+                },
+                error => {
+                    console.log("Failed");
+                }
+            );
+        this.candidateService.getAppliedJobs(this.id)
+            .pipe(first())
+            .subscribe(
+                (data: Job[]) => {
+                    this.appliedJobs = data;
+                    for(var i = 0 ; i < this.appliedJobs.length ; ++i) {
+                        this.appliedJobsId.push(this.appliedJobs[i].jobId);
+                    }
+                    console.log(this.appliedJobsId);
+                },
+                error => {
+                    console.log("Failed");
+                }
+            );
+    }
+    onBookmark(jobs: Job) {
+        this.candidateSaveJobDTO = new CandidateSaveJobsDTO();
+        this.candidateSaveJobDTO.candidateId = JSON.parse(localStorage.getItem("currentUser")).id;
+        console.log(jobs);
+        this.candidateSaveJobDTO.jobId = jobs.jobId;
+        console.log(this.candidateSaveJobDTO);
+        this.candidateService
+            .candidateSaveJob(this.candidateSaveJobDTO)
             .pipe(first())
             .subscribe(
                 (data: any) => {
@@ -124,16 +154,17 @@ export class JobComponent implements OnInit {
                 error => {
                     console.log("Fail");
                 }
-            );;
+            );
     }
 
     onApply(jobs: Job) {
         this.candidateSaveJobDTO = new CandidateSaveJobsDTO();
-        this.candidateSaveJobDTO.candidateId =  JSON.parse(localStorage.getItem("currentUser")).id;
+        this.candidateSaveJobDTO.candidateId = JSON.parse(localStorage.getItem("currentUser")).id;
         console.log(jobs);
-        this.candidateSaveJobDTO.jobId =  jobs.jobId;
+        this.candidateSaveJobDTO.jobId = jobs.jobId;
         console.log(this.candidateSaveJobDTO);
-        this.candidateService.candidateApplyJob(this.candidateSaveJobDTO)
+        this.candidateService
+            .candidateApplyJob(this.candidateSaveJobDTO)
             .pipe(first())
             .subscribe(
                 (data: any) => {
@@ -142,7 +173,6 @@ export class JobComponent implements OnInit {
                 error => {
                     console.log("Fail");
                 }
-            );;
+            );
     }
-
 }
