2a06d1fbe4c76285da568adf3f4fbe0812bbd07f
magnet <tgn296@gmail.com>
2019-05-26 17:48:10 +0700

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,6 +5,8 @@ import { Account } from "../model/account.model";
 import { Job } from "../model/job.model";
 import { JobService } from "../service/job.service";
 import { first } from "rxjs/operators";
+import { MajorService } from '../service/major.service';
+import { Major } from '../model/major.model';
 
 @Component({
     selector: "app-home",
@@ -12,7 +14,7 @@ import { first } from "rxjs/operators";
     styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-    constructor(private formBuilder: FormBuilder, private jobService: JobService) {}
+    constructor(private formBuilder: FormBuilder, private jobService: JobService, private majorService: MajorService) {}
 
     searchJobForm: FormGroup;
     account = new Account();
@@ -20,7 +22,7 @@ export class HomeComponent implements OnInit {
     jobList: Job[];
     page: number;
     pageSize: number;
-
+    majorList: Major[];
     onSubmit(buttonType): void {
         if (buttonType === "Search") {
             if (this.searchJobForm.get("keyword").value === "" && this.searchJobForm.get("location").value === "" && this.searchJobForm.get("career").value === "") {
@@ -50,6 +52,23 @@ export class HomeComponent implements OnInit {
                     this.jobList =  unorderedJobList.sort(function(a, b) {
                                         return a.soLuongView < b.soLuongView ? 1 : a.soLuongView > b.soLuongView ? -1 : 0;
                                     });
+                },
+                error => {
+                    console.log("Faild");
+                }
+            );
+        this.majorService
+            .getAllMajors()
+            .pipe(first())
+            .subscribe(
+                (data: Major[]) => {
+                    var unorderedMajorList = data;
+                    this.majorList =  unorderedMajorList.sort(function(a, b) {
+                                        return a.tinTuyenDung.length < b.tinTuyenDung.length ? 1 : a.tinTuyenDung.length > b.tinTuyenDung.length ? -1 : 0;
+                                    });
+                    if(this.majorList.length > 8){
+                        this.majorList.length = 8;
+                    }
                 },
                 error => {
                     console.log("Faild");
--- src/app/model/major.model.ts
+++ src/app/model/major.model.ts
@@ -1,4 +1,7 @@
+import { Job } from './job.model';
+
 export class Major {
     nganhId: string;
     tenNganh: string;
+    tinTuyenDung: Job[];
 }
